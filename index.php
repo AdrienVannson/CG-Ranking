680d75fe67cccd3cafc3af57ed65cdcd1d0546de
Adrien VANNSON <adrien.vannson@gmail.com>
2018-06-25 13:40:22 +0200

    Create a chart

--- index.php
+++ index.php
@@ -20,9 +20,13 @@
 
   <div class="container">
 
+    <canvas id="chart" width="400" height="200"></canvas>
 
   </div>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+  <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
+  <script src="script.js"></script>
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,41 @@
+window.chartColors = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(75, 192, 192)',
+  blue: 'rgb(54, 162, 235)',
+  purple: 'rgb(153, 102, 255)',
+  grey: 'rgb(201, 203, 207)'
+};
+
+var myChart = new Chart(document.getElementById('chart').getContext('2d'), {
+  type: 'line',
+  data: {
+    labels: ['1', '2', '3', '4', '5', '6', '7'],
+    datasets: [
+      {
+        label: 'Player 1',
+        data: [12, 19, 3, 5, 2, 3, 7],
+        backgroundColor: window.chartColors.red,
+        borderColor: window.chartColors.red,
+        fill: false
+      },
+      {
+        label: 'Player 2',
+        data: [1, 5, 9, 10, 15, 10, 12],
+        backgroundColor: window.chartColors.blue,
+        borderColor: window.chartColors.blue,
+        fill: false
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
+    }
+  }
+});
