3127d7648fa85671a7393a1ad1e8cc633fa2d3b2
Adrien VANNSON <adrien.vannson@gmail.com>
2018-06-25 13:39:53 +0200

    Include Materialize

--- index.php
+++ index.php
@@ -2,10 +2,27 @@
 <html>
 <head>
   <title>CG Ranks</title>
+
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
 <body>
 
-<h1>CG Ranks</h1>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo center">CG Ranks</a>
+      <ul id="nav-mobile" class="hide-on-med-and-down right">
+        <li><a>About</a></li>
+      </ul>
+    </div>
+  </nav>
 
+  <div class="container">
+
+
+  </div>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
 </body>
 </html>
