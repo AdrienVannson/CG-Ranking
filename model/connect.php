19fdb52445a4993d05351888183f3a5f387ad7bc
Adrien VANNSON <adrien.vannson@gmail.com>
2018-07-27 15:54:19 +0200

    Store db credentials in a config file

--- model/connect.php
+++ model/connect.php
@@ -1,7 +1,8 @@
 <?php
+include_once('../config.php');
 
 try {
-	$db = new PDO(); // HIDDEN
+	$db = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASSWORD);
 }
 catch (Exception $e) {
 	echo 'Error: can\'t connect to database';
