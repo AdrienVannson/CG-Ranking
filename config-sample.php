aa66265afe48fc46637947d63c51387bf530d927
Adrien VANNSON <adrien.vannson@gmail.com>
2018-10-10 20:57:24 +0200

    Delete useless config

--- config-sample.php
+++ config-sample.php
@@ -7,6 +7,4 @@ define('DB_USER', '');
 define('DB_PASSWORD', '');
 
 // Game
-define('GAME_NAME', 'coders-strike-back');
-define('IS_CHALLENGE', false);
 define('MIN_TIME_BETWEEN_UPDATES', 59*60); // 59 min
