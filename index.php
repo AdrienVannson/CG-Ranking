3f6c260dda4551f4ee3a8e5599eca6b0ca10d4af
Adrien VANNSON <adrien.vannson@protonmail.com>
2018-10-29 18:24:08 +0100

    Update script's version

--- index.php
+++ index.php
@@ -159,6 +159,6 @@ include_once(__DIR__.'/model/Game.class.php');
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
-    <script src="script.js?v=2"></script>
+    <script src="script.js?v=3"></script>
 </body>
 </html>
